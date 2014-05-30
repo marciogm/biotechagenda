cbc1084cc82931d51183188ed801828f7f07cb5b
marciogm <marciogiaxa@gmail.com>
2014-05-30 13:48:00 -0300

    Validates presence some attributes - nome - email_para_contato - data - descricao

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -1,2 +1,3 @@
 class Evento < ActiveRecord::Base
+  validates :nome, :email_para_contato, :data, :descricao, presence: true
 end
--- test/models/evento_test.rb
+++ test/models/evento_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
-class EventoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Evento do
+  
+  let(:evento_params) {{ nome: "Evento de Testes", email_para_contato: "contato@evento.com", 
+      data: "2014-05-30", descricao: "Eventos de teste"}}
+  
+  let(:evento) { Evento.new evento_params}
+  
+  it "is valid with valid params" do
+    evento.must_be :valid?
+  end
+  
+  it "is invalid without an nome" do
+    evento_params.delete :nome
+    
+    evento.wont_be :valid?
+    evento.errors[:nome].must_be :present?
+  end
+  
+  it "is invalid without an email para contato" do
+    evento_params.delete :email_para_contato
+    
+    evento.wont_be :valid?
+    evento.errors[:email_para_contato].must_be :present?
+  end
+  
+  it "is invalid without an Data" do
+    evento_params.delete :data
+    
+    evento.wont_be :valid?
+    evento.errors[:data].must_be :present?
+  end
+  
+  it "is invalid without an Descricao" do
+    evento_params.delete :descricao
+    
+    evento.wont_be :valid?
+    evento.errors[:descricao].must_be :present?
+  end
+  
 end
