3c316235c1d28db44f42d8ee86f891512462aad1
marciogm <marciogiaxa@gmail.com>
2014-06-02 14:54:28 -0300

    fix facebook js in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,10 @@
 $(function(){ $(document).foundation(); });
 
 
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&appId=1455759177970489&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+}(document, 'script', 'facebook-jssdk'));
