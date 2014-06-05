b04dd763e1413fe1cc91bcea5bcd9848e50a85a3
marciogm <marciogiaxa@gmail.com>
2014-06-04 23:41:39 -0300

    Removed facebook share button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,12 +17,3 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
-
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&appId=1455759177970489&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
