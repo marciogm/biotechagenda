74f0b2174f4c1f06a5d3e299660e6806f1c8cb56
marciogm <marciogiaxa@gmail.com>
2014-06-29 11:58:19 -0300

    Months in portuguese

--- app/helpers/eventos_helper.rb
+++ app/helpers/eventos_helper.rb
@@ -1,4 +1,15 @@
 module EventosHelper
+  BR_ABBR_MONTHNAMES = [nil] + %w(Jan Fev Mar Abr Mai Jun Jul Ago Set Out Nov Dez)
+  BR_MONTHNAMES = [nil] + %w(Janeiro Fevereiro Março Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro)
+
+  def abbr_month_name(month)
+      BR_ABBR_MONTHNAMES[month]
+  end
+  
+  def month_name(month)
+    BR_MONTHNAMES[month]
+  end
+  
   def url_with_protocol(url)
       /^http/.match(url) ? url : "http://#{url}"
     end
