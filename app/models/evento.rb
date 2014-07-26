fe4d769a8f09a97d847166b09dd5489bb6ae6257
Marcio Giaxa Marinheiro <marciogiaxa@gmail.com>
2014-07-25 21:18:46 -0300

    Show only current events

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -1,21 +1,25 @@
 class Evento < ActiveRecord::Base
   extend FriendlyId
   friendly_id :nome, :use => [:slugged, :finders]
-  
+
   mount_uploader :logo, LogoUploader
-  
+
   belongs_to :estado
   belongs_to :cidade
   belongs_to :categoria
   has_many :confirmas
-  
+
   validates :nome, :email_para_contato, :data, :descricao, :estado_id, :cidade_id, :categoria_id, presence: { message: "não pode ser vazio" }
   validates :email_para_contato, format: { with: /@/, message: "dever ser válido"}
-    
+
+  data = Date.today
+
   scope :nao_aprovados,         ->  { where(aprovado: false) }
   scope :aprovados,             ->  { where(aprovado: true) }
-  scope :aprovados_por_data,    ->  { where(aprovado: true).order("data ASC") }
-  
+  # scope :aprovados_por_data,    ->  { where(aprovado: true).order("data ASC") }
+  scope :aprovados_por_data,    ->  { where("aprovado = ? AND ((? between data and data_terminio) OR (data >= ?))", true, Date.today, Date.today)}
+  scope :aprovados_terminados,  -> { where("aprovado = ? AND ((? between data and data_terminio) OR (data <= ?))", true, Date.today, Date.today)}
+
   def aprova!
     self.aprovado = true
     self.update_attributes(:aprovado => true)
@@ -25,13 +29,13 @@ class Evento < ActiveRecord::Base
     self.aprovado = false
     self.update_attributes(:aprovado => false)
   end
-  
+
   def confirma_presenca(user)
     if usuario_aprovado? user
       confirma = Confirma.create!(evento_id: self.id, user_id: user.id)
     end
   end
-  
+
   def desconfirma_presenca(user)
     evento_confirmado = Confirma.where(evento_id: self.id, user_id: user.id).first
     evento_confirmado.destroy
