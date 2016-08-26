698f95b607cd2773a67b5237b1c50da59faaa685
marciogm <marciogiaxa@gmail.com>
2016-08-26 15:28:47 -0300

    added FactoryGirl

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'factory_girl'
 require "minitest/reporters"
 
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  include FactoryGirl::Syntax::Methods
+
+  FactoryGirl.definition_file_paths = [File.expand_path('../factories', __FILE__)]
+  FactoryGirl.find_definitions
 
   # Add more helper methods to be used by all tests here...
 end
