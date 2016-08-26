4df8891e1a054262262e9d7a96c66ef554e83007
marciogm <marciogiaxa@gmail.com>
2016-08-26 15:45:06 -0300

    Implement some methods for Events
    #new
    #create
    #destroy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,36 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:destroy]
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_path, notice: 'Event was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+    def event_params
+      params.require(:event).permit(:name, :description, :start_date, :end_date)
+    end
 end
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get new" do
+    get new_event_path
+    assert_response :success
+  end
+
+
+  test "should create event" do
+    assert_difference('Event.count') do
+      post events_path(params: { event: attributes_for(:event) })
+    end
+    assert_equal 'Event was successfully created.', flash[:notice]
+  end
+
+  test "should destroy event" do
+    event = create(:event)
+    assert_difference('Event.count', -1) do
+      delete event_path(event)
+    end
+
+    assert_redirected_to events_path
+    assert_equal 'Event was successfully destroyed.', flash[:notice]
+  end
+
 end
